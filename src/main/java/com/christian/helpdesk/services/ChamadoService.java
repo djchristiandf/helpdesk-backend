61d9ee2a7ca47b8117d0c4f647371dd850126761
djchristiandf <djchristiandf@gmail.com>
2022-09-23 19:51:30 -0300

    criado chamados findById e findAll - 23/09/2022 19:51

--- src/main/java/com/christian/helpdesk/services/ChamadoService.java
+++ src/main/java/com/christian/helpdesk/services/ChamadoService.java
@@ -1,5 +1,28 @@
 package com.christian.helpdesk.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.christian.helpdesk.domain.Chamado;
+import com.christian.helpdesk.repositories.ChamadoRepository;
+import com.christian.helpdesk.services.exceptions.ObjectNotFoundException;
+
+@Service
 public class ChamadoService {
 
+	@Autowired
+	private ChamadoRepository repository;
+	
+	public Chamado findById(Integer id) {
+		Optional<Chamado> obj= repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Chamado nao encontrado pelo Id: " +id));
+	}
+
+	public List<Chamado> findAll() {
+		return repository.findAll();
+	}
+	
 }
